b31468b5820f4868f5e35784fa51f34a9eba9207
Sani Serge <sergyth@gmail.com>
2026-06-03 16:53:28 +0200

    feat: centralisation des dépendances dans le Bootstrapper et configuration de FluentValidation

--- KineticTrack.Api/Program.cs
+++ KineticTrack.Api/Program.cs
@@ -1,9 +1,14 @@
+using KineticTrack.Bootstrapper;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplicationDependencies(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,28 +19,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
---
+++ KineticTrack.Bootstrapper/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using KineticTrack.Application.Services; 
+using KineticTrack.Infrastructure;       
+using KineticTrack.Security.Extensions;  
+
+namespace KineticTrack.Bootstrapper
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+            services.AddInfrastructureServices(configuration);
+            services.AddSecurityServices();
+            services.AddValidatorsFromAssemblyContaining<UserService>();         
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
