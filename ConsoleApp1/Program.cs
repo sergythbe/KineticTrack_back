eafe81dcc3cab5cb34215add04dfb3b6daded3e3
Serge Thierry SANI NOUNKWA <sergyth@gmail.com>
2026-08-08 11:22:31 +0200

    Delete ConsoleApp1 directory

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-using KineticTrack.Security.Services.Tools;
-
-var hasher = new PasswordHasherService();
-
-Console.WriteLine(hasher.Hash("Admin@2026"));
-// → colle ce résultat dans ton seed data
